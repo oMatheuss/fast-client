f591322b5918755bcb705da308a403e5a9a42126
Matheus <matheussmoura@outlook.com>
2023-12-23 15:48:29 -0300

    publish configs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,18 @@
-import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json' with { type: 'json' };
+import typescript from 'rollup-plugin-typescript2';
 import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/main.ts',
-  output: {
-    file: 'output/index.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
   plugins: [typescript(), terser()],
 };
